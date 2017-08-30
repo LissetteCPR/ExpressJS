13d5a1f2b5debfc69c840b143cf5576296a095c7
Lissette Ponguillo <liss_cpr@hotmail.com>
2017-08-30 16:01:23 -0500

    Application with ExpressJS

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-//require module express
-const express = require('express');
+const express = require('express'),
+      path = require('path'),
+      routes = require('./routes');
 
-express()
-  .use(express.static(__dirname + '/', {'index': ['index.html', 'index.html']}))
-  .listen(3000);
+const app = express();
 
-console.log('server running');
+//config
+app.set('view engine', 'pug');
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('port', process.env.PORT || 3000);
+
+//calling to homepage
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//routers
+app.use('/', routes);
+
+app.listen(app.get('port'), () => {
+  console.log('Server started port ' + app.get('port'));
+});
+
+
+
